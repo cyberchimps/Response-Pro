399443909c3e6ccc3ad9c2f861cefe71058d8c9c
Tyler Cunningham <tyler@cyberchimps.com>
2012-02-23 16:59:55 -0800

    removed legacy preg_match code

--- page.php
+++ page.php
@@ -24,7 +24,6 @@
 /* Define global variables. */
 	global $options, $post, $themeslug;
 	$size = get_post_meta($post->ID, 'page_slider_size' , true);
-	$nivosize = get_post_meta($post->ID, 'page_nivoslider_size' , true);
 	$page_section_order = get_post_meta($post->ID, 'page_section_order' , true);
 	if(!$page_section_order) {
 		$page_section_order = 'breadcrumbs,page_section';
@@ -32,19 +31,6 @@
 	
 /* End define global variables. */
 
-/* Set slider hook based on page option */
-
-if (preg_match("/page_slider/", $page_section_order ) && $size == "1" ) {
-	remove_action ('response_page_slider', 'response_slider_content' );
-	add_action ('response_page_content_slider', 'response_slider_content' );
-}
-
-if (preg_match("/page_nivoslider/", $page_section_order ) && $nivosize == "1" ) {
-	remove_action ('response_page_nivoslider', 'response_nivoslider_content' );
-	add_action ('response_page_content_slider', 'response_nivoslider_content' );
-}
-
-/* End set slider hook*/
 ?>
 
 <div class="container">
