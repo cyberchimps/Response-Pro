4143089ea3af6202d7485e411b87bd7468f739a4
Tyler Cunningham <tyler@cyberchimps.com>
2012-02-23 22:47:29 -0800

    ie sidebar css

--- core/widgets/tabbed-widget.php
+++ core/widgets/tabbed-widget.php
@@ -1,28 +1,28 @@
 <?php
 
-add_action('widgets_init', 'ifeature_tabbed_widget');
-add_image_size('ifeature-tabbed', 45, 45, true);
+add_action('widgets_init', 'response_tabbed_widget');
+add_image_size('response-tabbed', 45, 45, true);
 
-function ifeature_tabbed_widget() {
-	register_widget('iFeature_Tabbed_Widget');
+function response_tabbed_widget() {
+	register_widget('response_Tabbed_Widget');
 }
 
-class iFeature_Tabbed_Widget extends WP_Widget {
+class response_Tabbed_Widget extends WP_Widget {
 	function __construct() {
 		$widget_options = array(
-			'classname' => 'ifeature_tabbed_widget',
-			'description' => __('A tabbed widget that display popular posts, recent posts, comments and tags.', 'ifeature')
+			'classname' => 'response_tabbed_widget',
+			'description' => __('A tabbed widget that display popular posts, recent posts, comments and tags.', 'response')
 		);
 
 		$control_options = array(
 			'width' => 300,
 			'height' => 350,
-			'id_base' => 'ifeature_tabbed_widget'
+			'id_base' => 'response_tabbed_widget'
 		);
 
 		parent::__construct(
-			'ifeature_tabbed_widget',
-			__('iFeature Tabbed Widget', 'ifeature'),
+			'response_tabbed_widget',
+			__('response Tabbed Widget', 'response'),
 			$widget_options,
 			$control_options
 		);
@@ -47,16 +47,16 @@ class iFeature_Tabbed_Widget extends WP_Widget {
 		echo '</style>';
 	
 ?>
-<div class="ifeature-tabbed-widget">
-	<div class="ifeature-tabbed-wrap">
-		<ul class="ifeature-tabbed-header">
+<div class="response-tabbed-widget">
+	<div class="response-tabbed-wrap">
+		<ul class="response-tabbed-header">
 			<li class="first"><a href="#tab-1"><?php echo $instance['tab1'] ?></a></li>
 			<li class=""><a href="#tab-2"><?php echo $instance['tab2'] ?></a></li>
 			<li class=""><a href="#tab-3"><?php echo $instance['tab3'] ?></a></li>
 			<li class="last"><a href="#tab-4"><?php echo $instance['tab4'] ?></a></li>
 		</ul>
-		<div class="ifeature-tabbed-tab" id="tab-1">
-			<ul class="ifeature-tabbed-popular-posts">
+		<div class="response-tabbed-tab" id="tab-1">
+			<ul class="response-tabbed-popular-posts">
 				<?php
 					$popular_posts = new WP_Query();
 					$popular_posts->query(array('ignore_sticky_posts' => '1', 'posts_per_page' => 5, 'orderby' => 'comment_count'));
@@ -65,14 +65,14 @@ class iFeature_Tabbed_Widget extends WP_Widget {
 					?>
 						<li>
 							<div class="tab-image">
-								<a href="<?php the_permalink() ?>"><?php the_post_thumbnail("ifeature-tabbed") ?>
+								<a href="<?php the_permalink() ?>"><?php the_post_thumbnail("response-tabbed") ?>
 							</div>
 
 							<div class="details">
 								<h5 class="tabbed-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
 								<span class="tabbed-date">
 									<?php the_time(get_option('date_format')) ?>,
-									<?php comments_popup_link(__('No comments', 'ifeature'), __('1 Comment', 'ifeature'), __('% Comments', 'ifeature')); ?>
+									<?php comments_popup_link(__('No comments', 'response'), __('1 Comment', 'response'), __('% Comments', 'response')); ?>
 								</span>
 							</div>
 						</li>
@@ -82,8 +82,8 @@ class iFeature_Tabbed_Widget extends WP_Widget {
 			</ul>
 		</div><!-- #tab-1 -->
 
-		<div class="ifeature-tabbed-tab" id="tab-2">
-			<ul class="ifeature-tabbed-recent-posts">
+		<div class="response-tabbed-tab" id="tab-2">
+			<ul class="response-tabbed-recent-posts">
 				<?php
 					$recent_posts = new WP_Query();
 					$recent_posts->query(array('ignore_sticky_posts' => '1', 'posts_per_page' => 5));
@@ -92,14 +92,14 @@ class iFeature_Tabbed_Widget extends WP_Widget {
 					?>
 						<li>
 							<div class="tab-image" >
-								<a href="<?php the_permalink() ?>"><?php the_post_thumbnail("ifeature-tabbed") ?>
+								<a href="<?php the_permalink() ?>"><?php the_post_thumbnail("response-tabbed") ?>
 							</div>
 
 							<div class="details">
 								<h5 class="tabbed-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
 								<span class="tabbed-date">
 									<?php the_time(get_option('date_format')) ?>,
-									<?php comments_popup_link(__('No comments', 'ifeature'), __('1 Comment', 'ifeature'), __('% Comments', 'ifeature')); ?>
+									<?php comments_popup_link(__('No comments', 'response'), __('1 Comment', 'response'), __('% Comments', 'response')); ?>
 								</span>
 							</div>
 						</li>
@@ -109,28 +109,28 @@ class iFeature_Tabbed_Widget extends WP_Widget {
 			</ul>
 		</div><!-- #tab-2 -->
 
-		<div class="ifeature-tabbed-tab" id="tab-3">
-			<ul class="ifeature-tabbed-comments"> 
+		<div class="response-tabbed-tab" id="tab-3">
+			<ul class="response-tabbed-comments"> 
 				<?php foreach(get_comments(array('number' => 5)) as $comment): ?>
 					<?php $post = get_post($comment->comment_post_ID); ?>
 					<li>
 						<div class="tab-image">
-						<a href="<?php echo get_permalink($post->ID); ?>#comment-<?php echo $comment->comment_ID; ?>" title="<?php echo strip_tags($comment->comment_author); ?> <?php _e('on ', 'ifeature'); ?><?php echo $post->post_title; ?>"><?php echo get_avatar( $comment, '45' ); ?></a>
+						<a href="<?php echo get_permalink($post->ID); ?>#comment-<?php echo $comment->comment_ID; ?>" title="<?php echo strip_tags($comment->comment_author); ?> <?php _e('on ', 'response'); ?><?php echo $post->post_title; ?>"><?php echo get_avatar( $comment, '45' ); ?></a>
 						</div>
 
 						<div class="details">
-						<h5 class="tabbed-title"><a href="<?php echo get_permalink($post->ID); ?>#comment-<?php echo $comment->comment_ID; ?>" title="<?php echo strip_tags($comment->comment_author); ?> <?php _e('on ', 'ifeature'); ?><?php echo $post->post_title; ?>"><?php echo strip_tags($comment->comment_author); ?>: <?php echo substr(strip_tags($comment->comment_content), 0, 50); ?>...</a></h5>
+						<h5 class="tabbed-title"><a href="<?php echo get_permalink($post->ID); ?>#comment-<?php echo $comment->comment_ID; ?>" title="<?php echo strip_tags($comment->comment_author); ?> <?php _e('on ', 'response'); ?><?php echo $post->post_title; ?>"><?php echo strip_tags($comment->comment_author); ?>: <?php echo substr(strip_tags($comment->comment_content), 0, 50); ?>...</a></h5>
 						</div>
 					</li>
 				<?php endforeach; ?>
 			</ul>
 		</div><!-- #tab-3 -->
 
-		<div class="ifeature-tabbed-tab" id="tab-4">
+		<div class="response-tabbed-tab" id="tab-4">
 			<?php wp_tag_cloud(array('largest' => 12, 'smallest' => 12, 'unix' => 'px')) ?>
 		</div><!-- #tab-4 -->
-	</div> <!-- .ifeature-tabbed-wrap -->
-</div> <!-- .ifeature-tabbed-widget -->
+	</div> <!-- .response-tabbed-wrap -->
+</div> <!-- .response-tabbed-widget -->
 </div> <!-- end tabbed widget container -->
 	<?php
 		wp_reset_query();
