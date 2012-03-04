df93d7e7786b12014384f2242bded45040324fb6
Tyler Cunningham <tyler@cyberchimps.com>
2012-03-03 16:27:05 -0800

    fixed typo, quirks mode thing

--- includes/classy-options-init.php
+++ includes/classy-options-init.php
@@ -76,7 +76,7 @@ $options
 <p>Thank you for using Response Pro.</p>
 ")
 	->section("Design")
-		->subsection("Typopgraphy")
+		->subsection("Typography")
 			->select($themeslug."_font", "Choose a Font", array( 'options' => array("Arial" => "Arial (default)", "Courier New" => "Courier New", "Georgia" => "Georgia", "Helvetica" => "Helvetica", "Lucida Grande" => "Lucida Grande", "Tahoma" => "Tahoma", "Times New Roman" => "Times New Roman", "Verdana" => "Verdana", "Actor" => "Actor", "Coda" => "Coda", "Maven+Pro" => "Maven Pro", "Metrophobic" => "Metrophobic", "News+Cycle" => "News Cycle", "Nobile" => "Nobile", "Tenor+Sans" => "Tenor Sans", "Quicksand" => "Quicksand", "Ubuntu" => "Ubuntu", 'custom' => "Custom")))
 			->text($themeslug."_custom_font", "Enter a Custom Font")
 						->textarea($themeslug."_typekit", "TypeKit Code")
