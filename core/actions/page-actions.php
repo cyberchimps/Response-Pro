743a1960e1d1a9d37ad5a24a9f88cb21e89ff959
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-25 17:11:44 -0700

    working on other templates now

--- core/actions/page-actions.php
+++ core/actions/page-actions.php
@@ -35,14 +35,14 @@ function response_page_section_content() {
 
 
 ?>
-<div class="row">
+<div class="row-fluid">
 	<!--Begin @response before content sidebar hook-->
 		<?php response_before_content_sidebar(); ?>
 	<!--End @response before content sidebar hook-->
 			
 		<div id="content" class="<?php echo $content_grid; ?>">
 		
-		<?php response_page_content_slider(); ?>
+	
 		
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
