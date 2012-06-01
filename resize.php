6f8c1fdbd51e2f752ea4ed70bfe4a6eeeb27614c
Tyler Cunningham <Sanchez@Tylers-MacBook-Air.local>
2012-06-01 10:12:10 -0700

    undid test changes

--- resize.php
+++ resize.php
@@ -7,8 +7,8 @@
 
 	global $options, $themeslug, $post, $sidebar, $content_grid, $root; // call globals	
 
-	$source = "http://farm3.static.flickr.com/2156/2364129390_4fe946df4a.jpg";
-	$image = wp_resize( '', $source , 1000, 1000, true );
+	$source = "$root/library/images/fish.jpg";
+	$image = wp_resize( '', $source , 400, 100, true );
 
 get_header();  //Display Header
 
