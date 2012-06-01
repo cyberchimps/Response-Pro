ef99f6ccc9bae2700ac7c1ba1e6e332c6f56a51f
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-31 18:15:42 -0700

    Pro library and functions

---
+++ pro/init.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Initializes the CyberChimps Response Core Framework Pro Extension
+*
+* Author: Tyler Cunningham
+* Copyright: © 2012
+* {@link http://cyberchimps.com/ CyberChimps LLC}
+*
+* Released under the terms of the GNU General Public License.
+* You should have received a copy of the GNU General Public License,
+* along with this software. In the main directory, see: /licensing/
+* If not, see: {@link http://www.gnu.org/licenses/}.
+*
+* @package Response
+* @since 1.0
+*/
+
+require_once ( get_template_directory() . '/pro/hooks.php' );
+require_once ( get_template_directory() . '/pro/functions.php' );
+
+// Load element files
+require_once ( get_template_directory() . '/core/elements/slider.php' );
+
+?>
