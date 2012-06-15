e92a009d54b9961f3aaf9f92102bf2a2b7b9d3ba
Tyler Cunningham <tyler@cyberchimps.com>
2012-06-14 17:31:46 -0700

    duplicate code for single and footer

--- core/actions/header-actions.php
+++ core/actions/header-actions.php
@@ -40,7 +40,7 @@ add_action( 'response_404_content', 'response_404_content_handler' );
 add_action( 'response_logo_icons', 'response_logo_icons_content');
 add_action( 'response_custom_header_element', 'response_custom_header_element_content');
 add_action( 'response_logo_register', 'response_logo_register_content');
-add_action( 'response_header_banner_element', 'response_header_banner_element_content');
+
 
 /**
 * Establishes the theme font family.
@@ -458,40 +458,6 @@ function response_logo_icons_content() {
 <?php
 }
 
-/**
-* Header Banner Element
-*
-* @since 2.0
-*/
-function response_header_banner_element_content() {
-global $themeslug, $options, $root; //Call global variables
-
-	$banner_type = $options->get($themeslug.'_header_banner_type'); //Type of header banner 
-	$affiliate_img = $options->get($themeslug.'_header_banner_affiliate_image'); //Affiate banner image
-	$affiliate_url = $options->get($themeslug.'_header_banner_affiliate_link'); //Affiliate banner link
-	$img_src = $options->get($themeslug.'_header_banner_image'); //User uploaded image
-	$img_url = $options->get($themeslug.'_header_banner_url'); //URL for user uploaded image
-	$embed = $options->get($themeslug.'_header_banner_code_embed'); //Embed code for external ad.
-
-?>
-	<div class="span12">
-		<div id="banner">
-			
-			<?php if ($banner_type == "key1"):?>
-				<a href="<?php echo $affiliate_url; ?>/"><img src="http://placehold.it/1020x120" alt="Affiliate"></a>		
-			<?php endif; ?>
-			
-			<?php if ($banner_type == "key2"):?>
-				<a href="<?php echo $img_url; ?>/"><img src="<?php echo $img_src; ?>" alt="logo"></a>		
-			<?php endif; ?>
-			<?php if ($banner_type == "key3") { echo stripslashes($embed); } ?>
-			
-			
-		</div>		
-	</div>	
-
-<?php
-}
 
 /**
 * End
